f9db7ebb83705d078ccd497e2dea320f1c6f32a4
Ryn <31045220+vmRyn@users.noreply.github.com>
2021-02-08 10:51:25 +0000

    Removal of JSON, Adding of JS Config File.

---
+++ js/master/config.js
@@ -0,0 +1,19 @@
+var config = {
+    "text": {
+        "LargeName": "RYAN DEMPSEY",
+        "TopMainButton": "E-Portfolio",
+        "BottomMainButton": "My Github",
+        "RevealButton": "Reveal Website",
+        "SendFormButton": "Send Form",
+        "NavName": "Ryan Dempsey",
+        "NavAbout": "About",
+        "NavSkills": "Skills",
+        "NavProjects": "Projects",
+        "NavContact": "Contact",
+        "HeaderAbout": "ABOUT ME",
+        "HeaderSkills": "MY SKILLS",
+        "HeaderProjects": "PROJECTS",
+        "HeaderContact": "CONTACT ME",
+        "Footer": "© DESIGNED BY RYN"
+    }
+}
---
+++ js/master/elements.js
@@ -0,0 +1,15 @@
+document.getElementById('LargeName').innerHTML = config.text.LargeName;
+document.getElementById('TopMainButton').innerHTML = config.text.TopMainButton;
+document.getElementById('BottomMainButton').innerHTML = config.text.BottomMainButton;
+document.getElementById('RevealButton').innerHTML = config.text.RevealButton;
+document.getElementById('SendFormButton').innerHTML = config.text.SendFormButton;
+document.getElementById('NavName').innerHTML = config.text.NavName;
+document.getElementById('NavAbout').innerHTML = config.text.NavAbout;
+document.getElementById('NavSkills').innerHTML = config.text.NavSkills;
+document.getElementById('NavProjects').innerHTML = config.text.NavProjects;
+document.getElementById('NavContact').innerHTML = config.text.NavContact;
+document.getElementById('HeaderAbout').innerHTML = config.text.HeaderAbout;
+document.getElementById('HeaderSkills').innerHTML = config.text.HeaderSkills;
+document.getElementById('HeaderProjects').innerHTML = config.text.HeaderProjects;
+document.getElementById('HeaderContact').innerHTML = config.text.HeaderContact;
+document.getElementById('Footer').innerHTML = config.text.Footer;
